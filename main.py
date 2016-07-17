9e65497f0ef8f469f0fb6de2415592bbe9ea95d7
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-07-17 19:15:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def challenger():
+  one = 'Please provide your first number'
+  a = input(one)
+  int(a)
+  two = 'Please provide your second number'
+  b = input(two)
+  int(b)
+  three = 'Please provide the third number'
+  c = input(three)
+  int(c)
+  four = 'Please provide the final number'
+  n = input(four)
+  int(n)
+  #this is where the inputs are stored
+  def challenge()
+      return a, b, c, n
+  challenger = check_fermat(challenge)
